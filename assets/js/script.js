c5c0274a6315981d97c65f759558016e95dfef0d
David García González <github@davidgg.dev>
2024-09-02 19:28:00 +0200

    first commit: added all files

---
+++ assets/js/script.js
@@ -0,0 +1,88 @@
+// window scroll
+$(window).on("scroll", function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 100) {
+        $("body").addClass("fixed-header");
+    }else {
+        $("body").removeClass("fixed-header");
+    }
+
+    if (scroll >= 4400) {
+        document.querySelectorAll('[data-scroll-index]').forEach(el => el.classList.remove('active'));
+        document.querySelector('[data-scroll-index="6"]').classList.add('active');
+    }
+});
+
+// Document Ready
+$(document).ready(function () {
+
+    // Typing animation
+    new Typed('#type-it', {
+        strings: ['Backend<br>Developer'],
+        typeSpeed: 100,
+    });
+
+    //Owl Carousel
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        items:2,
+        margin:30,
+        autoplay:true,
+        autoplayTimeout:2000,
+        responsive:{
+            0:{
+                items:1
+            },
+            900:{
+                items:2
+            },
+        }
+    });
+
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const sections = document.querySelectorAll('section[data-scroll-index]');
+  const navItems = document.querySelectorAll('[data-scroll-nav]');
+
+  function onScroll() {
+    let scrollPosition = window.scrollY + window.innerHeight / 2;
+
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+      const sectionIndex = section.getAttribute('data-scroll-index');
+
+      if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+        navItems.forEach(navItem => {
+          navItem.classList.remove('active');
+          if (navItem.getAttribute('data-scroll-nav') === sectionIndex) {
+            navItem.classList.add('active');
+          }
+        });
+      }
+    });
+  }
+
+  function onClickNavItem(event) {
+    event.preventDefault();
+    const targetIndex = event.target.getAttribute('data-scroll-nav');
+    const targetSection = document.querySelector(`section[data-scroll-index="${targetIndex}"]`);
+
+    if (targetSection) {
+      const targetPosition = targetSection.offsetTop;
+      window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth'
+      });
+    }
+  }
+
+  navItems.forEach(navItem => {
+    navItem.addEventListener('click', onClickNavItem);
+  });
+
+  window.addEventListener('scroll', onScroll);
+  onScroll(); // Llamar a la función para establecer la sección activa al cargar la página
+});
+
