c0543bd28ca58a9df539789b04733ff37fdc6e2c
David García González <github@davidgg.dev>
2024-09-13 19:57:33 +0200

    feat:Add English CV and update script for language-based CV download

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,18 @@ const textsToChange = document.querySelectorAll("[data-section");
 const changeLanguage = async language =>{
   const requestJson = await fetch(`./languages/${language}.json`);
   const texts = await requestJson.json();
+  const cvLink = document.querySelector('[data-lang="cv"]');
 
   for(const textToChange of textsToChange){
     const section = textToChange.dataset.section;
     const value = textToChange.dataset.value;
 
     textToChange.innerHTML = texts[section][value];
+  }
+  if (language === 'es') {
+    cvLink.href = 'assets/downloads/CV - David García González - ES.pdf';
+  } else {
+    cvLink.href = 'assets/downloads/CV - David García González - EN.pdf';
   }
 }
 
